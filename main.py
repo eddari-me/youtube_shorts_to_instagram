6d46885f0943470d2d822e18346c25ab2c45efef
eddari-me <104611333+eddari-me@users.noreply.github.com>
2023-05-03 16:21:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+# main.py
+
+from youtube.youtube_api import YoutubeAPI
+from instagram.instagram_uploader import InstagramUploader
+
+
+def main():
+    youtube_api = YoutubeAPI()
+    instagram_uploader = InstagramUploader()
+
+    youtube_api.authenticate()
+    instagram_uploader.instagram_api.authenticate()
+
+    instagram_uploader.download_latest_short_video()
+    instagram_uploader.upload_latest_video_to_instagram()
+
+
+if __name__ == '__main__':
+    main()
